5f6d88b7f089a3434d99c5787e38217bb118def4
NidhiAbhyka <nidhiabhyankar@mail.com>
2021-07-30 18:53:43 +0530

    updated ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartsComponent } from './components/charts/charts.component';
 import { FxOrderRatesComponent } from './components/fx-order-rates/fx-order-rates.component';
+
 import { NewsComponent } from './components/news/news.component';
 import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: 'news', component: NewsComponent },
   { path: 'tools', component: FxOrderRatesComponent },
   { path: 'chart', component: ChartsComponent },
-  // { path: 'productDetails', component: ProductDetailsComponent },
+  // { path: 'home', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
